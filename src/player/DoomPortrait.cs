bd1eed28bed349600cbd0944248e66099e762acf
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-07-07 23:40:31 +0200

    Startup fixes

--- src/player/DoomPortrait.cs
+++ src/player/DoomPortrait.cs
@@ -2,108 +2,108 @@ using Godot;
 
 public enum DoomPortraitType
 {
-    Idle,
-    Treasure,
-    Pain,
-    Death,
-    Flying
+	Idle,
+	Treasure,
+	Pain,
+	Death,
+	Flying
 }
 
 // reason for this class: animated sprites don't fit into controls well,
 // rather just set it up manually
 // this also makes it easier to fine tune animations compared to adding 4 of the same frame
-public partial class DoomPortrait : Node3D
+public partial class DoomPortrait : PanelContainer
 {
-    private static readonly (int frame, float secondsPerFrame, int yOffset)[][] _animations = {
+	private static readonly (int frame, float secondsPerFrame, int yOffset)[][] _animations = {
 		// Idle
 		new (int, float, int)[] {
-            (1, 0.5f, 0), (2, 0.3f, 0), (1, 0.5f, 0), (0, 0.3f, 0),
-        },
+			(1, 0.5f, 0), (2, 0.3f, 0), (1, 0.5f, 0), (0, 0.3f, 0),
+		},
 		// Treasure
 		new (int, float, int)[] {
-            (3, 0.2f, 0), (4, 0.2f, 0), (5, 0.2f, 0),
-        },
+			(3, 0.2f, 0), (4, 0.2f, 0), (5, 0.2f, 0),
+		},
 		// Pain
 		new (int, float, int)[] {
-            (6, 0.3f, 0), (-1, 0.3f, 0),
-        },
+			(6, 0.3f, 0), (-1, 0.3f, 0),
+		},
 		// Death
 		new (int, float, int)[] {
-            (7, 2.0f, 0), (-1, 2.0f, 0),
-        },
+			(7, 2.0f, 0), (-1, 2.0f, 0),
+		},
 		// Flying
 		new (int, float, int)[] {
-            (9, 0.1f, 0), (10, 0.1f, -1), (11, 0.1f, 0),
-        },
+			(9, 0.1f, 0), (10, 0.1f, -1), (11, 0.1f, 0),
+		},
 
-    };
+	};
 
-    private static readonly Vector2 Seperation = new Vector2(4, 5);
-    private static readonly Vector2 FrameSize = new Vector2(49, 46);
-    private const int FramesPerRow = 3;
+	private static readonly Vector2 Seperation = new Vector2(4, 5);
+	private static readonly Vector2 FrameSize = new Vector2(49, 46);
+	private const int FramesPerRow = 3;
 
-    private AtlasTexture _portraitAtlas;
-    [Export] private DoomPortraitType _activePortrait = DoomPortraitType.Idle;
-    private int _currentIndex;
-    private double _timer;
+	private AtlasTexture _portraitAtlas;
+	[Export] private DoomPortraitType _activePortrait = DoomPortraitType.Idle;
+	private int _currentIndex;
+	private double _timer;
 
-    public override void _Ready()
-    {
-        _portraitAtlas = (AtlasTexture)GetNode<TextureRect>("MarginContainer/TextureRect").Texture;
+	public override void _Ready()
+	{
+		_portraitAtlas = (AtlasTexture)GetNode<TextureRect>("MarginContainer/TextureRect").Texture;
 
-        UpdateAtlas();
-    }
+		UpdateAtlas();
+	}
 
-    public void SetAnimation(DoomPortraitType type)
-    {
-        if (_activePortrait == type)
-        {
-            return;
-        }
+	public void SetAnimation(DoomPortraitType type)
+	{
+		if (_activePortrait == type)
+		{
+			return;
+		}
 
-        _activePortrait = type;
+		_activePortrait = type;
 
-        (_currentIndex, _timer) = (0, 0);
-        UpdateAtlas();
-    }
+		(_currentIndex, _timer) = (0, 0);
+		UpdateAtlas();
+	}
 
-    public override void _Process(double delta)
-    {
-        (int frame, float secondsPerFrame, int yOffset)[] activeAnimation = _animations[(int)_activePortrait];
+	public override void _Process(double delta)
+	{
+		(int frame, float secondsPerFrame, int yOffset)[] activeAnimation = _animations[(int)_activePortrait];
 
-        _timer += delta;
+		_timer += delta;
 
-        bool didAnimationUpdate = false;
+		bool didAnimationUpdate = false;
 
-        while (activeAnimation[_currentIndex].secondsPerFrame < _timer)
-        {
-            _timer -= activeAnimation[_currentIndex].secondsPerFrame;
-            _currentIndex = (_currentIndex + 1) % activeAnimation.Length;
-            didAnimationUpdate = true;
-        }
+		while (activeAnimation[_currentIndex].secondsPerFrame < _timer)
+		{
+			_timer -= activeAnimation[_currentIndex].secondsPerFrame;
+			_currentIndex = (_currentIndex + 1) % activeAnimation.Length;
+			didAnimationUpdate = true;
+		}
 
-        if (didAnimationUpdate)
-        {
-            UpdateAtlas();
-        }
-    }
+		if (didAnimationUpdate)
+		{
+			UpdateAtlas();
+		}
+	}
 
-    private void UpdateAtlas()
-    {
-        (int frame, _, int yOffset) = _animations[(int)_activePortrait][_currentIndex];
+	private void UpdateAtlas()
+	{
+		(int frame, _, int yOffset) = _animations[(int)_activePortrait][_currentIndex];
 
-        if (frame < 0)
-        {
-            _portraitAtlas.Region = new Rect2(0, 0, 0, 0);
-        }
-        else
-        {
-            int row = frame % FramesPerRow;
-            int column = frame / FramesPerRow;
+		if (frame < 0)
+		{
+			_portraitAtlas.Region = new Rect2(0, 0, 0, 0);
+		}
+		else
+		{
+			int row = frame % FramesPerRow;
+			int column = frame / FramesPerRow;
 
-            Vector2 position = Seperation + (Seperation + FrameSize) * new Vector2(row, column) + new Vector2(0, yOffset);
+			Vector2 position = Seperation + (Seperation + FrameSize) * new Vector2(row, column) + new Vector2(0, yOffset);
 
-            _portraitAtlas.Region = new Rect2(position, FrameSize);
-        }
-    }
+			_portraitAtlas.Region = new Rect2(position, FrameSize);
+		}
+	}
 }
--- src/ratgrunt/RatGrunt.cs
+++ src/ratgrunt/RatGrunt.cs
@@ -5,170 +5,170 @@ namespace PawsPlunder;
 
 public partial class RatGrunt : CharacterBody3D, IMeleeTargettable, IBulletHittable, IDeathPlaneEnterable, IPlayerAttacker, IMoveable
 {
-    public event Action? Died;
+	public event Action? Died;
 
-    [Export] private PackedScene _bulletScene = null!;
-    [Export] private PackedScene _droppedGunScene = null!;
+	[Export] private PackedScene _bulletScene = null!;
+	[Export] private PackedScene _droppedGunScene = null!;
 
-    private const float BulletSpeed = 30.0f;
+	private const float BulletSpeed = 30.0f;
 
-    private const float FarAwaySpeed = 12.0f;
-    private const float NearbySpeed = 4.0f;
-    private const float WhatIsNearby = 5.0f;
-    private const float ChanceForIdleSwing = 0.2f;
-    private const int ShootFrame = 1;
-    private static readonly Distro _footstepsPitchDistro = (1.0f, 0.2f);
-    private static readonly Distro _attackBarkPitchDistro = (1.0f, 0.2f);
-    private static readonly Distro _firePitchDistro = (1.0f, 0.2f);
-    private static readonly Distro _deathPitchDistro = (1.0f, 0.2f);
+	private const float FarAwaySpeed = 12.0f;
+	private const float NearbySpeed = 4.0f;
+	private const float WhatIsNearby = 5.0f;
+	private const float ChanceForIdleSwing = 0.2f;
+	private const int ShootFrame = 1;
+	private static readonly Distro _footstepsPitchDistro = (1.0f, 0.2f);
+	private static readonly Distro _attackBarkPitchDistro = (1.0f, 0.2f);
+	private static readonly Distro _firePitchDistro = (1.0f, 0.2f);
+	private static readonly Distro _deathPitchDistro = (1.0f, 0.2f);
 
-    [Export] private NavigationAgent3D _agent = null!;
-    [Export] private AnimatedSprite3D _sprite = null!;
-    [Export] private Node3D _centerOfMass = null!;
-    [Export] private RatGruntSounds _sounds = null!;
+	[Export] private NavigationAgent3D _agent;
+	[Export] private AnimatedSprite3D _sprite;
+	[Export] private Node3D _centerOfMass;
+	[Export] private RatGruntSounds _sounds;
 
-    public Vector3 CenterOfMass => _centerOfMass.GlobalPosition;
+	public Vector3 CenterOfMass => _centerOfMass.GlobalPosition;
 
-    private bool _hasMoveTarget = false;
-    private Vector3? _queuedAttackDirection = null;
+	private bool _hasMoveTarget = false;
+	private Vector3? _queuedAttackDirection = null;
 
-    public override void _Ready()
-    {
-        _sprite.Play("Idle");
-        _sprite.Frame = Globals.Rng.RandiRange(0, _sprite.FrameCount() - 1);
-    }
+	public override void _Ready()
+	{
+		_sprite.Play("Idle");
+		_sprite.Frame = Globals.Rng.RandiRange(0, _sprite.FrameCount() - 1);
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        FollowPath(delta);
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		FollowPath(delta);
+	}
 
-    private void FollowPath(double delta)
-    {
-        if (!_hasMoveTarget)
-        {
-            return;
-        }
+	private void FollowPath(double delta)
+	{
+		if (!_hasMoveTarget)
+		{
+			return;
+		}
 
-        if (_queuedAttackDirection != null)
-        {
-            return;
-        }
+		if (_queuedAttackDirection != null)
+		{
+			return;
+		}
 
-        Vector3 nextPosition = _agent.GetNextPathPosition();
-        Vector3 targetPosition = _agent.TargetPosition;
+		Vector3 nextPosition = _agent.GetNextPathPosition();
+		Vector3 targetPosition = _agent.TargetPosition;
 
-        bool isNearby = GlobalPosition.DistanceTo(targetPosition) < WhatIsNearby;
+		bool isNearby = GlobalPosition.DistanceTo(targetPosition) < WhatIsNearby;
 
-        float speed = isNearby ? NearbySpeed : FarAwaySpeed; 
-        GlobalPosition = GlobalPosition.MoveToward(nextPosition, speed * (float)delta);
+		float speed = isNearby ? NearbySpeed : FarAwaySpeed; 
+		GlobalPosition = GlobalPosition.MoveToward(nextPosition, speed * (float)delta);
 
-        _hasMoveTarget = !_agent.IsNavigationFinished();
+		_hasMoveTarget = !_agent.IsNavigationFinished();
 
-        if (!_hasMoveTarget)
-        {
-            _sounds.Footsteps.Stop();
-            _sprite.Play("Idle");
-        }
-    }
+		if (!_hasMoveTarget)
+		{
+			_sounds.Footsteps.Stop();
+			_sprite.Play("Idle");
+		}
+	}
 
-    public void GoTo(Vector3 point)
-    {
-        _hasMoveTarget = true;
-        _agent.TargetPosition = point;
+	public void GoTo(Vector3 point)
+	{
+		_hasMoveTarget = true;
+		_agent.TargetPosition = point;
 
-        _sounds.Footsteps.PlayPitched(_footstepsPitchDistro);
-        _sprite.Play("Walk");
-    }
+		_sounds.Footsteps.PlayPitched(_footstepsPitchDistro);
+		_sprite.Play("Walk");
+	}
 
-    public void AttackTarget(Player target)
-    {
-        _queuedAttackDirection = CenterOfMass.DirectionTo(target.CenterOfMass) * BulletSpeed;
-        
-        _sprite.Play("Shoot");
-        _sounds.AttackBark.PlayPitched(_attackBarkPitchDistro);
-    }
+	public void AttackTarget(Player target)
+	{
+		_queuedAttackDirection = CenterOfMass.DirectionTo(target.CenterOfMass) * BulletSpeed;
+		
+		_sprite.Play("Shoot");
+		_sounds.AttackBark.PlayPitched(_attackBarkPitchDistro);
+	}
 
-    private void OnAnimationFrameChanged()
-    {
-        if (_sprite.Animation == "Shoot" && _sprite.Frame == ShootFrame && _queuedAttackDirection != null)
-        {
-            ShootBulletWithVelocity(_queuedAttackDirection.Value);
+	private void OnAnimationFrameChanged()
+	{
+		if (_sprite.Animation == "Shoot" && _sprite.Frame == ShootFrame && _queuedAttackDirection != null)
+		{
+			ShootBulletWithVelocity(_queuedAttackDirection.Value);
 
-            _sounds.Fire.PlayPitched(_firePitchDistro);
-        }
-    }
+			_sounds.Fire.PlayPitched(_firePitchDistro);
+		}
+	}
 
-    private void OnAnimationFinished()
-    {
-        if (_sprite.Animation == "Idle")
-        {
-            float chanceToSwing = Globals.Rng.Randf();
+	private void OnAnimationFinished()
+	{
+		if (_sprite.Animation == "Idle")
+		{
+			float chanceToSwing = Globals.Rng.Randf();
 
-            if (chanceToSwing < ChanceForIdleSwing)
-            {
-                _sprite.Play("IdleSwordSwing");                
-            }
-        }
-        else if (_sprite.Animation == "IdleSwordSwing")
-        {
-            _sprite.Play("Idle");
-        }
-        else if (_sprite.Animation == "Shoot")
-        {
-            _queuedAttackDirection = null;
+			if (chanceToSwing < ChanceForIdleSwing)
+			{
+				_sprite.Play("IdleSwordSwing");                
+			}
+		}
+		else if (_sprite.Animation == "IdleSwordSwing")
+		{
+			_sprite.Play("Idle");
+		}
+		else if (_sprite.Animation == "Shoot")
+		{
+			_queuedAttackDirection = null;
 
-            if (_hasMoveTarget)
-            {
-                _sprite.Play("Walk");
-                _sounds.Footsteps.PlayPitched(_footstepsPitchDistro);
-            }
-            else
-            {
-                _sprite.Play("Idle");
-            }
-        }
-    }
+			if (_hasMoveTarget)
+			{
+				_sprite.Play("Walk");
+				_sounds.Footsteps.PlayPitched(_footstepsPitchDistro);
+			}
+			else
+			{
+				_sprite.Play("Idle");
+			}
+		}
+	}
 
-    private void ShootBulletWithVelocity(Vector3 velocity)
-    {
-        Bullet bullet = _bulletScene.Instantiate<Bullet>();        
-        GetParent().AddChild(bullet);
-        bullet.GlobalPosition = CenterOfMass;
-        bullet.Initialize(this, velocity, PhysicsLayers3D.World | PhysicsLayers3D.Player);
-    }
+	private void ShootBulletWithVelocity(Vector3 velocity)
+	{
+		Bullet bullet = _bulletScene.Instantiate<Bullet>();        
+		GetParent().AddChild(bullet);
+		bullet.GlobalPosition = CenterOfMass;
+		bullet.Initialize(this, velocity, PhysicsLayers3D.World | PhysicsLayers3D.Player);
+	}
 
-    void IMeleeTargettable.Target(MeleeHitInfo info)
-    {
-        DestroyModel();
-    }
+	void IMeleeTargettable.Target(MeleeHitInfo info)
+	{
+		DestroyModel();
+	}
 
-    void IBulletHittable.Hit(BulletHitInfo info)
-    {
-        DestroyModel();
-    }
+	void IBulletHittable.Hit(BulletHitInfo info)
+	{
+		DestroyModel();
+	}
 
-    private void DestroyModel()
-    {
-        Died?.Invoke();
+	private void DestroyModel()
+	{
+		Died?.Invoke();
 
-        CollisionLayer = 0;
-        CollisionMask = 0;
-        _hasMoveTarget = false;
-        _queuedAttackDirection = null;
+		CollisionLayer = 0;
+		CollisionMask = 0;
+		_hasMoveTarget = false;
+		_queuedAttackDirection = null;
 
-        Node3D droppedGun = _droppedGunScene.Instantiate<Node3D>();
-        GetParent().AddChild(droppedGun);
-        droppedGun.GlobalPosition = CenterOfMass;
-        
-        _sprite.Play("Death");
-        _sounds.Death.PlayPitched(_deathPitchDistro);
-        _sounds.Footsteps.Stop();
-    }
+		Node3D droppedGun = _droppedGunScene.Instantiate<Node3D>();
+		GetParent().AddChild(droppedGun);
+		droppedGun.GlobalPosition = CenterOfMass;
+		
+		_sprite.Play("Death");
+		_sounds.Death.PlayPitched(_deathPitchDistro);
+		_sounds.Footsteps.Stop();
+	}
 
-    void IDeathPlaneEnterable.EnteredDeathPlane()
-    {
-        Died?.Invoke();
-        QueueFree();
-    }
+	void IDeathPlaneEnterable.EnteredDeathPlane()
+	{
+		Died?.Invoke();
+		QueueFree();
+	}
 }
